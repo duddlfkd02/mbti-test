842f9f30fa184541eb28e90ff7c427adb9625abe
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-09-07 23:04:33 +0900

    add: 기본 form 기능 작성 완료

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,5 +1,53 @@
-const AuthForm = () => {
-  return <div>AuthForm</div>;
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthForm = (mode) => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    id: "",
+    password: "",
+    nickname: mode === "signup" ? "" : null,
+  });
+
+  const handleChange = (e) => {
+    const inputName = e.target.name; //name속성 이용해서 동적인 값 저장하기 (id, password, nickname)
+    const inputValue = e.target.value; //input에 입력하는 값
+    setFormData({
+      ...formData,
+      [inputName]: inputValue,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="id"
+        value={formData.id}
+        onChange={handleChange}
+        placeholder="아이디"
+        required
+      />
+      {mode === "signup" && (
+        <input
+          type="text"
+          name="nickname"
+          value={formData.nickname}
+          onChange={handleChange}
+          placeholder="닉네임"
+          required
+          className="w-full p-4 border border-gray-300 rounded-lg"
+        />
+      )}
+
+      <button type="button">{mode === "login" ? "로그인" : "회원가입"}</button>
+    </form>
+  );
 };
 
 export default AuthForm;
